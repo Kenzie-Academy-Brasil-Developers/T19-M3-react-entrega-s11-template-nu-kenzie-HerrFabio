81c412c1d3abd8e65a32ebc99877a1687189958e
Fabio <fabioalbertoaraujo@gmail.com>
2023-10-22 23:58:17 -0300

    T19-M3-react-entrega-s11-template-nu-kenzie-HerrFabio

--- src/pages/LandingPages/index.jsx
+++ src/pages/LandingPages/index.jsx
@@ -2,17 +2,17 @@ import { DefaultTemplate } from "../../components/DefaultTemplate/index.jsx";
 import { FinanceForm } from "../../components/FinanceForm/index.jsx"
 import { FinanceList } from "../../components/FinanceList/index.jsx";
 import { Total } from "../../components/FinanceForm/Total/index.jsx";
-
+import styles from "./style.module.scss";
 
 export const LandingPages = ({financeInfo, addFinanceInfo, deleteCard, financeCard}) => {
     return (
-        <>
+        <main className={styles.mainBox}>
             <DefaultTemplate>
                 <FinanceForm  addFinanceInfo={addFinanceInfo}/>
                 <Total financeInfo={financeInfo} />
                 <FinanceList financeInfo={financeInfo} deleteCard={deleteCard} financeCard={financeCard}/>
             </DefaultTemplate>
-        </>
+        </main>
     
     );
 };
